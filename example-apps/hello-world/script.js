9b99c58c5bef1eb31a2f81c416164be31b8323c9
Daniel Rasmuson <dan123911@gmail.com>
2017-04-07 12:26:24 -0600

    Added example apps folder

---
+++ example-apps/hello-world/script.js
@@ -0,0 +1,3 @@
+new DroneDeploy({version: 1}).then(function(api){
+  console.log('DroneDeploy Api: ', api);
+});
---
+++ example-apps/recommended-starting-template/script.js
@@ -0,0 +1,22 @@
+dronedeploy.onload(function(){
+  console.log('DroneDeploy Api: ', dronedeploy);
+});
+
+//Expandable section
+var isExpanded = false;
+var upArrow = 'https://s3.amazonaws.com/drone-deploy-plugins/templates/login-example-imgs/arrow-up.svg';
+var downArrow = 'https://s3.amazonaws.com/drone-deploy-plugins/templates/login-example-imgs/arrow-down.svg';
+var expandArrow = document.querySelector('.expand-arrow');
+var expandBody = document.querySelector('.expand-section');
+var expandRow = document.querySelector('.expand-row');
+
+expandRow.addEventListener('click', function(){
+  isExpanded = !isExpanded
+  if (isExpanded){
+    expandArrow.src = upArrow;
+    expandBody.style.display = 'block';
+  } else{
+    expandArrow.src = downArrow;
+    expandBody.style.display = 'none';
+  }
+});
